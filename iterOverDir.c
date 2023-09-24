2a23a1a06b0780d8d78fa362dbf0800036f38fd9
master-saz <145714482+master-saz@users.noreply.github.com>
2023-09-24 22:32:12 +0200

    Update iterOverDir.c
    read fopen

--- iterOverDir.c
+++ iterOverDir.c
@@ -46,7 +46,29 @@ int main(void)
       strcat(destination,newsource);
       char* fullpath = destination;
       printf("Full Path = %s\n", fullpath);
-      //fp = fopen ("file.txt", "r");
+      // pointer demo to FILE
+      FILE* demo;
+      int display;
+   
+      // Creates a file "demo_file"
+      // with file access as read mode
+      demo = fopen(fullpath, "r");
+   
+      // loop to extract every characters
+      while (1) {
+          // reading file
+          display = fgetc(demo);
+          // end of file indicator
+          if (feof(demo))
+              break;
+          // displaying every characters
+          printf("%c", display);
+      }
+   
+      // closes the file pointed by demo
+      fclose(demo);
+
+      
       
       subdirectory = opendir(fullpath);
       if (subdirectory != NULL){
